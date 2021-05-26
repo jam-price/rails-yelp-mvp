2e7acdce0aeb5136ff50daa225391efeef4f9692
James Price <jamesmpdev@gmail.com>
2021-05-26 14:48:12 +0100

    adding create reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # /restaurant
   resources :restaurants do
-    resources :reviews, only: [:show, :new]
+    resources :reviews, only: [:new, :create]
   end
 
 end
