df278684152332cbe503fda20c42023d360bbe6a
James Price <jamesmpdev@gmail.com>
2021-05-26 12:00:48 +0100

    adding validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+    has_many :reviews, dependent: :destroy
+
+    CATEGORIES = %w(chinese italian japanese french belgian)
+    validates :address, presence: true
+    validates :name, presence: true
+    validates :category, presence: true
+    validates :category, inclusion: { in: CATEGORIES }
 end
